269715de181a15277abcfbd933018d5e815d0c93
Christopher Reinken <reinkendigital@gmail.com>
2021-10-04 11:11:13 -0500

    created default empty board as game state

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 class Game extends React.Component {
+    state = {
+        board: [['','',''],['','',''],['','','']]
+    };
+    
     render() {
         return (
             <div className="Game"></div>
