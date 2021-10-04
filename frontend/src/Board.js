8ccf3ae377ad72f8b0b47518577c7de1a2f210d2
Christopher Reinken <reinkendigital@gmail.com>
2021-10-04 11:54:32 -0500

    created full board layout

--- frontend/src/Board.js
+++ frontend/src/Board.js
@@ -4,14 +4,28 @@ const Board = (props) => {
     function createSquare(value) {
         return <BoardSquare value={value} />
     }
+
     const player = 'X';
 
     return (
         <div className="Game-Board">
             <div className="player">Current Player: {player}</div>
             <div className="board-row">
-                {this.createSquare({props.board[0][0]})}
+                {createSquare(props.board[0][0])}
+                {createSquare(props.board[0][1])}
+                {createSquare(props.board[0][2])}
             </div>
+            <div className="board-row">
+                {createSquare(props.board[1][0])}
+                {createSquare(props.board[1][1])}
+                {createSquare(props.board[1][2])}
+            </div>
+            <div className="board-row">
+                {createSquare(props.board[2][0])}
+                {createSquare(props.board[2][1])}
+                {createSquare(props.board[2][2])}
+            </div>
+
         </div>
     );
 }
