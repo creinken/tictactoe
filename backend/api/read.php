db165af16bf60ce931fbb7bc86663501b5507ea3
Christopher Reinken <reinkendigital@gmail.com>
2021-10-08 01:12:53 -0500

    main api read class

--- backend/api/read.php
+++ backend/api/read.php
@@ -1,25 +1,48 @@
 <?php
-//headers
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
 
-//initialize api
-include_once('../core/initialize.php');
+    // Headers
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
 
-//instantiate games
+    include_once '../config/database.php';
+    include_once '../class/games.php';
 
-$game = new Games($db);
+    $database = new Database();
+    $db = $database->getConnection();
 
-//get games
-$result = $game->read();
-//get row count
-$num = $result->rowCount();
+    $items = new Game($db);
 
-if($num > 0){
-    $game_arr = array();
-    $game_arr['data'] = array();
-}else{
+    $stmt = $items->getGames();
+    $itemCount = $stmt->rowCount();
 
-}
+    echo json_encode($itemCount);
+
+    if($itemCount > 0){
+
+        $gameArr = array();
+        $gameArr["body"] = array();
+        $gameArr["itemCount"] = $itemCount;
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            $e = array(
+                "id" => $id,
+                "playerX" => $playerX,
+                "playerO" => $playerO,
+                "current_player" => $current_player,
+                "game_over" => $game_over
+            );
+
+            array_push($gameArr["body"], $e);
+        }
+        echo json_encode($gameArr);
+    }
+
+    else{
+        http_response_code(404);
+        echo json_encode(
+            array("message" => "No record found.")
+        );
+    }
 
 ?>
