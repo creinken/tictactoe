b4539c4bfefbd1744ac602079f8f02d9fe8bd625
Christopher Reinken <reinkendigital@gmail.com>
2021-10-10 10:28:51 -0500

    added delete route

---
+++ backend/api/delete.php
@@ -0,0 +1,34 @@
+<?php
+
+    // headers
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-headers, Authorization, X-Requested-With");
+
+    include_once '../config/database.php';
+    include_once '../class/games.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $item = new Game($db);
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $item->id = $data->id;
+
+    // game values
+    $item->playerX = $data->playerX;
+    $item->playerO = $data->playerO;
+    $item->current_player = $data->current_player;
+    $item->game_over = $data->game_over;
+
+    if ($item->deleteGame()) {
+        echo 'Game deleted.';
+    } else {
+        echo 'Data could not be deleted.';
+    }
+
+?>
