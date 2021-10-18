a7354214de9519ce18be61c6ce34188e183174e3
Christopher Reinken <reinkendigital@gmail.com>
2021-10-18 11:12:44 -0500

    created game and move and added 1 to many relation

---
+++ backend/database/migrations/2021_10_18_160618_create_moves_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moves', function (Blueprint $table) {
+            $table->id();
+            $table->squareX();
+            $table->squareY();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moves');
+    }
+}
